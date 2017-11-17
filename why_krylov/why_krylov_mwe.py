8f52672c9ac2ecd8e7192868ef004e547ee4ab8b
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2017-11-17 11:18:36 +0100

    Minimized

--- why_krylov/why_krylov_mwe.py
+++ why_krylov/why_krylov_mwe.py
@@ -11,26 +11,14 @@ mesh = fenics.UnitSquareMesh(8, 8)
 
 x = fenics.SpatialCoordinate(mesh)
 
-exact_solution = 1. + x[0]**2 + 2.*x[1]**2
-
-f = -6.
-
 V = fenics.FunctionSpace(mesh, 'P', 2)
-    
-bc = fenics.DirichletBC(V, exact_solution, boundary)
 
-u = fenics.TrialFunction(V)
-
-v = fenics.TestFunction(V)
-
-dot, grad = fenics.dot, fenics.grad
-
-a = dot(grad(v), grad(u))*fenics.dx
-
-L = v*f*fenics.dx
-
-solution = fenics.Function(V)
+u, v = fenics.TrialFunction(V), fenics.TestFunction(V)
 
 fenics.set_log_level(fenics.DEBUG)
 
-fenics.solve(a == L, solution, bc)
+dot, grad = fenics.dot, fenics.grad
+
+fenics.solve(dot(grad(v), grad(u))*fenics.dx == v*-6.*fenics.dx,
+    fenics.Function(V),
+    fenics.DirichletBC(V, 1. + x[0]**2 + 2.*x[1]**2, boundary))
